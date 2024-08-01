050c8acf12521f5554e18eb7dbb84138fde903b7
MyungHoon Ju <myunghoonju@gmail.com>
2024-08-02 00:56:47 +0900

    delete

--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -1,4 +1,6 @@
 const { User } = require('../models/User')
+const {Blog} = require("../models/Blog");
+const {Comment} = require('../models/Comment')
 const  Router  = require('express');
 const mongo= require("mongoose");
 const userRouter = Router();
@@ -44,19 +46,37 @@ userRouter.delete('/:id', async (req, res) => {
         return res.status(404).send({ err: 'invalid id' })
     }
 
-    let user = await User.deleteOne({ _id: id });
+   const user = await Promise.all([
+        User.findOneAndDelete({ _id: id }),
+        Blog.deleteMany({ "user._id": id }),
+        Blog.updateMany({"comments.user": id}, {$pull: { comments: { user:id } } }),
+        Comment.deleteMany({ user: id })
+    ])
+
     return res.send({ user })
 })
 
 userRouter.put('/:id', async (req, res) => {
     const id = req.params.id;
-    const {email, username} = req.body;
+    const name = req.body;
     if (!mongo.isValidObjectId(id)) {
         return res.status(404).send({ err: 'invalid id' })
     }
 
-    let user = await User.findByIdAndUpdate(id, { $set: { email, username }}, { new: true });
+    let user = await User.findById(id);
+    // not working
+    if (name) {
+        user.name = name
+        await Promise.all([
+            await Blog.updateMany({"user._id": id}, { "user": name }),
+            await Blog.updateMany(
+                {},
+                {"comments.$[comment].userFullName": `${name.first} ${name.last}`},
+                { arrayFilters: [{"comment.user": id}] })
+        ])
+    }
 
+    await user.save()
     return res.send({ user })
 })
 
